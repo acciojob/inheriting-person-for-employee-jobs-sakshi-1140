a9d9c8891bb9a7f17a2eecbbabe5c73cafd36da8
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-19 23:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// complete this js code
-class Person {
-	 constructor(name,age){
+class Person{
+  constructor(name,age){
     this.name=name;
     this.age=age;
   }
@@ -9,14 +8,14 @@ class Person {
   }
 }
 
-class Employee extends Person {
-	constructor(name,age,jobTitle){
+class Employee extends Person{
+  constructor(name,age,jobTitle){
     super(name,age)
     this.jobTitle=jobTitle;
   }
   jobGreet(){
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-  } 
+  }  
 }
 
 // Do not change code below this line
